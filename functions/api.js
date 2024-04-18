843cef0987c8599e721b5f16f41c30763ddafe7e
thehungrycoder225 <cyber_sky09@yahoo.com>
2024-04-18 11:31:38 +0800

    minor changes

--- functions/api.js
+++ functions/api.js
@@ -1,50 +1,17 @@
 const express = require('express');
 const serverless = require('serverless-http');
+import router from './routes/author.js';
+
 const app = express();
-const router = express.Router();
 
-let records = [];
-
-//Get all students
-router.get('/', (req, res) => {
-  res.send('App is running..');
-});
-
-//Create new record
-router.post('/add', (req, res) => {
-  res.send('New record added.');
-});
-
-//delete existing record
-router.delete('/', (req, res) => {
-  res.send('Deleted existing record');
-});
-
-//updating existing record
-router.put('/', (req, res) => {
-  res.send('Updating existing record');
-});
-
-//showing demo records
-router.get('/demo', (req, res) => {
-  res.json([
-    {
-      id: '001',
-      name: 'Smith',
-      email: 'smith@gmail.com',
-    },
-    {
-      id: '002',
-      name: 'Sam',
-      email: 'sam@gmail.com',
-    },
-    {
-      id: '003',
-      name: 'lily',
-      email: 'lily@gmail.com',
-    },
-  ]);
-});
+mongoose
+  .connect(
+    'mongodb+srv://cryx25:enovation25@cluster0.mdjfo.azure.mongodb.net/testDB' ||
+      'mongodb://localhost:27017/express-mongo-api',
+    {}
+  )
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error('Failed to connect to MongoDB', error));
 
 app.use('/.netlify/functions/api', router);
 module.exports.handler = serverless(app);
