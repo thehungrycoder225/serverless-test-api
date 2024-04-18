5d63bca6cd5826713924c1c6580e2eb501a987cd
thehungrycoder225 <cyber_sky09@yahoo.com>
2024-04-18 11:21:15 +0800

    minor changes

--- functions/api.js
+++ functions/api.js
@@ -1,15 +1,50 @@
-import express from 'express';
-import serverless from 'serverless-http';
-
-const api = express();
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
 const router = express.Router();
 
+let records = [];
+
+//Get all students
 router.get('/', (req, res) => {
-  res.send({
-    hello: 'express',
-  });
+  res.send('App is running..');
 });
 
-api.use('/.netlify/functions/api', router);
+//Create new record
+router.post('/add', (req, res) => {
+  res.send('New record added.');
+});
 
-export const handler = serverless(api);
+//delete existing record
+router.delete('/', (req, res) => {
+  res.send('Deleted existing record');
+});
+
+//updating existing record
+router.put('/', (req, res) => {
+  res.send('Updating existing record');
+});
+
+//showing demo records
+router.get('/demo', (req, res) => {
+  res.json([
+    {
+      id: '001',
+      name: 'Smith',
+      email: 'smith@gmail.com',
+    },
+    {
+      id: '002',
+      name: 'Sam',
+      email: 'sam@gmail.com',
+    },
+    {
+      id: '003',
+      name: 'lily',
+      email: 'lily@gmail.com',
+    },
+  ]);
+});
+
+app.use('/.netlify/functions/api', router);
+module.exports.handler = serverless(app);
